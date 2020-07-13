49438af65bb1f843fe61c1927d663e5448e83b8b
unknown <alexei24@list.ru>
2020-07-13 20:20:03 +0300

    tehtävät 2.15.-2.18

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
 
-const Persons = ({persons, searchedName}) => {
+
+const Persons = ({persons, searchedName, removeData}) => {
   
   const filtered = () => persons.filter((person) => person.name.toLowerCase().includes(searchedName.toLowerCase()))
 
   return(    
     filtered().map(person => 
-    <p key={person.name}>{person.name} {person.number}</p>
+    <p key={person.name}>{person.name} {person.number} 
+    <button onClick={() => removeData(person)}>remove</button>
+    </p>
+    
     )
   )
 }
@@ -45,16 +50,19 @@ const NewDataForm = ({addData, newName, newNumber, handleNameChange, handleNumbe
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [searchedName, setSearchedName ] = useState('')
 
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+  
   const addData = (event) => {
     event.preventDefault()
 
@@ -64,13 +72,49 @@ const App = () => {
     }
 
     if (persons.find(person => person.name === newData.name)) {
-      window.alert(`${newName} is already added to phonebook`);
+      if (window.confirm(`${newData.name} is already added to phonebook, replace the old number with the new one?`)) {
+        persons.forEach(person => {
+          if (person.name === newData.name) {
+            changeData(person.id, newData.number)
+          }
+        })
+      }
+      
     }
     else{
-      setPersons(persons.concat(newData))
+      personService
+      .create(newData)
+      .then(sentData => {
+        setPersons(persons.concat(sentData))
+        setNewName('')
+        setNewNumber('')
+      }) 
+    }
+  }
+
+  const removeData = (person) => {
+    const id = person.id
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      personService
+        .remove(id)
+        .then(success => {
+          setPersons(persons.filter(person => person.id !== id))
+        }) 
+    }
+  }
+
+  const changeData = (id, newNumber) => {
+    const person = persons.find(p => p.id === id)
+    const changedData = { ...person, number: newNumber }
+
+    personService
+      .update(id, changedData)
+        .then(returnedData => {
+        setPersons(persons.map(person => person.id !== id ? person : returnedData))
+      })
       setNewName('')
       setNewNumber('')
-    }
+      
   }
 
   const searchName = (event) => {
@@ -85,9 +129,7 @@ const App = () => {
     setNewNumber(event.target.value)
   }
   const handleSearch = (event) => {
-    
     setSearchedName(event.target.value)
-
   }
   
   return (
@@ -100,7 +142,8 @@ const App = () => {
         newName={newName} handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} searchedName={searchedName} />
+      <Persons persons={persons} searchedName={searchedName}
+      removeData={removeData} />
       
       
     </div>
