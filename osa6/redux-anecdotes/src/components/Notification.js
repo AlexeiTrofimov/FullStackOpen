cb769902279f92f38e0ce683679db41990195e6d
unknown <alexei24@list.ru>
2020-07-29 16:33:35 +0300

    Tehtävät 6.19-6.21

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notifications)
+const Notification = (props) => {
+  const notification =  props.notifications
   const style = {
     border: 'solid',
     padding: 10,
@@ -18,4 +18,11 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notifications: state.notifications,
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,14 +1,18 @@
 const initialState = null
+var timeoutID
 
 export const setNotification= (content, time) => {
+  
   return async dispatch => {
     dispatch({
       type: 'SET_NOTIFICATION',
       data: content
     })
-    setTimeout(() => {
+    clearTimeout(timeoutID)
+    timeoutID = setTimeout(() => {
       dispatch({ type: 'CLEAR_NOTIFICATION' })
     }, time*1000)
+    console.log("second: ",timeoutID)
   }
 }
 
